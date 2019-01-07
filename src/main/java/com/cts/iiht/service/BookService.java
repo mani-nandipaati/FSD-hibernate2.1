8a5e6f830ec966d809314f7e5f39d2d3427b1825
mani <nandipaati.mani@gmail.com>
2019-01-07 07:55:58 +0530

    First Commit

---
+++ src/main/java/com/cts/iiht/service/BookService.java
@@ -0,0 +1,43 @@
+package com.cts.iiht.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.cts.iiht.dao.BookDao;
+import com.cts.iiht.entity.Book;
+
+@Service
+@Transactional
+public class BookService {
+	
+	@Autowired
+	BookDao bookDao;
+	
+	
+	public void addBook(Book book ) {
+		bookDao.addBook(book);
+	}
+	
+	public void deleteBook(long id) {
+		bookDao.deleteBook(id);
+	}
+
+	public Book searchBook(long id) {
+		return bookDao.searchBook(id);
+	}
+
+	public List<Book> getAllBooks(){
+		return bookDao.getAllBooks();
+	}
+	
+	public List<Book> getAllBooksSortByTitle(){
+		return bookDao.getAllBooksSortByTitle();
+	}
+	
+	public List<Book> getAllBooksSortByPublishDate(){
+		return bookDao.getAllBooksSortByPublishDate();
+	}
+}
---
+++ src/main/java/com/cts/iiht/service/SubjectService.java
@@ -0,0 +1,39 @@
+package com.cts.iiht.service;
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.cts.iiht.dao.SubjectDao;
+import com.cts.iiht.entity.Subject;
+
+@Service
+@Transactional
+public class SubjectService {
+	
+	@Autowired
+	SubjectDao subjectDao;
+	
+	
+	public void addSubject(Subject subject) {
+		subjectDao.addSubject(subject);
+	}
+	
+	public void deleteSubject(long id) {
+		subjectDao.deleteSubject(id);
+	}
+
+	public Subject searchSubject(long id) {
+		return subjectDao.searchSubject(id);
+	}
+	public List<Subject> getAllSubjects() {
+		return subjectDao.getAllSubjects();
+	}
+
+	public List<Subject> getAllSubjectsSortBySubTitle(){
+		return subjectDao.getAllSubjectsSortBySubTitle();
+	}
+}
